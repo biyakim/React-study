c13d4084d318bc51387bec7e2e4804d83c2e1f54
biyakim <w2116@e-mirim.hs.kr>
2023-02-10 00:17:54 +0900

    조건부 렌더링

--- my-app/src/chapter_01/Hello.jsx
+++ my-app/src/chapter_01/Hello.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 function Hello(props) {
-    return <div style={{color: props.color}}>안녕하세요 {props.name}</div>
+    return <div style={{color: props.color}}>
+        {props.isSpecial && <b>*</b>}
+        안녕하세요 {props.name}
+        </div>
 }
 
 Hello.defaultProps = {
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Wrapper>
-      <Hello name="React" color="red" />
+      <Hello name="React" color="red" isSpecial/>
       <Hello color="pink" />
     </Wrapper>
     
