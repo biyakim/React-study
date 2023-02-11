b5e04cb69a3c6184d73715afe88ca0f878bf2fde
rainya <98528278+biyakim@users.noreply.github.com>
2023-02-11 15:25:22 +0000

    todoList 만들기

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
 function TodoItem (props) {
-  const onRemove = () => {
-    
-  }
+ 
   return (
     <div className="todo-item">
       {props.item}
-       <button onClick={onRemove}>삭제</button>
     </div>
   )
 }
