9395e4146e07fa6e8f2bf717ffc407a75774a10a
biyakim <w2116@e-mirim.hs.kr>
2023-02-13 21:50:43 +0900

    useState 사용하기2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,18 +3,24 @@ import {Routes, Route, Link} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Counter from "./pages/Counter";
+import Input from "./pages/Input";
+import Input2 from "./pages/Input2";
 
 function App() {
   return (
     <div className="App">
       <nav>
         <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
-        <Link to="/counter">Counter</Link>
+        <Link to="/counter">Counter</Link> |{" "}
+        <Link to="/Input">Input</Link> | {" "}
+        <Link to="/Input2">Input2</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/counter" element={<Counter/>}/>
+        <Route path="/Input" element={<Input/>}/>
+        <Route path="/Input2" element={<Input2/>}/>
       </Routes>     
     </div>
   );
